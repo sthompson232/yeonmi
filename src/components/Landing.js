85995b93eefdf29d36fd5d5163c205532f35e627
Sam Thompson <samthompson292@gmail.com>
2021-06-16 19:26:38 +0100

    subtitle

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,6 +10,7 @@ export const Landing = () => {
     
     return (
         <div className='landing'>
+            <h2 className='story-of'><i>The Story of</i></h2>
             <h1 className='title'>Yeonmi Park</h1>
             <hr/>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
